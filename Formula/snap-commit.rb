9eba0bb11aa9b3b1862c7694487e21fee64f2324
nxkh4ng <kh4ngnx@gmail.com>
2026-06-14 16:02:43 +0700

    chore: update snap-commit to v0.5.0

--- Formula/snap-commit.rb
+++ Formula/snap-commit.rb
@@ -1,27 +1,27 @@
 class SnapCommit < Formula
-  desc "A lightweight CLI tool for Convention Commits"
+  desc "A lightweight CLI tool for Conventional Commits"
   homepage "https://github.com/nxkh4ng/snap-commit"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/nxkh4ng/snap-commit/releases/download/v0.4.0/snap_0.4.0_darwin_arm64.tar.gz"
-      sha256 "1f26336ca8600f0c26041712587f19a47d005435075a296fd8e90e5a2dab5d4a"
+      url "https://github.com/nxkh4ng/snap-commit/releases/download/v0.5.0/snap_0.5.0_darwin_arm64.tar.gz"
+      sha256 "29c7b66b98ed859fa19221d0680611e9370b7a73668df8fe9bcf8b5e9738fbd2"
     end
     on_intel do
-      url "https://github.com/nxkh4ng/snap-commit/releases/download/v0.4.0/snap_0.4.0_darwin_amd64.tar.gz"
-      sha256 "b66ca584e8603bc864e274e6abf8e12f1bb0ee63958f192c3cefd5fe7ffada35"
+      url "https://github.com/nxkh4ng/snap-commit/releases/download/v0.5.0/snap_0.5.0_darwin_amd64.tar.gz"
+      sha256 "3557c89a1370ecad85591a96099b061375f2655b4a885803e9de0cb293dece28"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/nxkh4ng/snap-commit/releases/download/v0.4.0/snap_0.4.0_linux_arm64.tar.gz"
-      sha256 "923ccb985095204710c0f7acd3e885cf2f7c2a9b9f32a4de33c4d807a531d998"
+      url "https://github.com/nxkh4ng/snap-commit/releases/download/v0.5.0/snap_0.5.0_linux_arm64.tar.gz"
+      sha256 "21c25f3ca47597d97f1ed236130479836bd9939a900ff076f530cbab0c5d8cca"
     end
     on_intel do
-      url "https://github.com/nxkh4ng/snap-commit/releases/download/v0.4.0/snap_0.4.0_linux_amd64.tar.gz"
-      sha256 "ff101588c859105338bcadf5aeaf8e87f6cadaae5b8418ab9c1044536c4d83f7"
+      url "https://github.com/nxkh4ng/snap-commit/releases/download/v0.5.0/snap_0.5.0_linux_amd64.tar.gz"
+      sha256 "db7dfe963e88d66ead3fe4152496c05f43f12fc68bedff15fa9b9e7bbb791aba"
     end
   end
 
