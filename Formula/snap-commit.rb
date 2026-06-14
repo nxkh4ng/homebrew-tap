9a4f772e1aef69013e8780e6662dad45d5fe3a50
Nguyễn Xuân Khang <nxkh4ng@users.noreply.github.com>
2026-06-14 10:19:24 +0000

    chore: update snap-commit to v0.5.1

--- Formula/snap-commit.rb
+++ Formula/snap-commit.rb
@@ -5,23 +5,23 @@ class SnapCommit < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/nxkh4ng/snap-commit/releases/download/v0.5.0/snap_0.5.0_darwin_arm64.tar.gz"
-      sha256 "29c7b66b98ed859fa19221d0680611e9370b7a73668df8fe9bcf8b5e9738fbd2"
+      url "https://github.com/nxkh4ng/snap-commit/releases/download/v0.5.1/snap_0.5.1_darwin_arm64.tar.gz"
+      sha256 "b925b00fce82c05a11f1a78ef142eaa2a9f29119233a1ede2f41e3b787e4dc78"
     end
     on_intel do
-      url "https://github.com/nxkh4ng/snap-commit/releases/download/v0.5.0/snap_0.5.0_darwin_amd64.tar.gz"
-      sha256 "3557c89a1370ecad85591a96099b061375f2655b4a885803e9de0cb293dece28"
+      url "https://github.com/nxkh4ng/snap-commit/releases/download/v0.5.1/snap_0.5.1_darwin_amd64.tar.gz"
+      sha256 "4a03e270d99e8925a766d1c33bb765d74d7053a03bacff1e274e43cc190fe4df"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/nxkh4ng/snap-commit/releases/download/v0.5.0/snap_0.5.0_linux_arm64.tar.gz"
-      sha256 "21c25f3ca47597d97f1ed236130479836bd9939a900ff076f530cbab0c5d8cca"
+      url "https://github.com/nxkh4ng/snap-commit/releases/download/v0.5.1/snap_0.5.1_linux_arm64.tar.gz"
+      sha256 "4aa27fec98a5d60f14e3d617e957a1661f9d55592ad15c4f73407682b1cfb0c2"
     end
     on_intel do
-      url "https://github.com/nxkh4ng/snap-commit/releases/download/v0.5.0/snap_0.5.0_linux_amd64.tar.gz"
-      sha256 "db7dfe963e88d66ead3fe4152496c05f43f12fc68bedff15fa9b9e7bbb791aba"
+      url "https://github.com/nxkh4ng/snap-commit/releases/download/v0.5.1/snap_0.5.1_linux_amd64.tar.gz"
+      sha256 "f2f6621c0ad53aaaa73974070379d9417c01bf7c718ea5e578f1f406cba958bd"
     end
   end
 
